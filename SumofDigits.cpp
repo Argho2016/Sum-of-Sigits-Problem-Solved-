f6d85f85c6208e53088805bd3283c435679f26fe
Argho Das <aiub2016@yahoo.com>
2020-04-08 16:45:48 +0600

    Source Code File

---
+++ SumofDigits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int t,n,a;
+cin>>t;
+
+for (int i=0; i<t; i++){
+
+    cin>>n;
+    for (int j; j<n; i++){
+    cin>>a;
+    a+=a;
+    cout<<a;
+    }
+}
+
+return 0;
+}
